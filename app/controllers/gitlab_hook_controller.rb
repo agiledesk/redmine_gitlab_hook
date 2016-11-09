0e37fcf6f2f407fe77cd65fbb4a431f52e887612
aleksandr1592 <aleksandr1592@gmail.com>
2016-11-09 10:32:52 +0300

    Update gitlab_hook_controller.rb

--- app/controllers/gitlab_hook_controller.rb
+++ app/controllers/gitlab_hook_controller.rb
@@ -5,6 +5,7 @@ class GitlabHookController < ActionController::Base
   GIT_BIN = Redmine::Configuration['scm_git_command'] || 'git'
   skip_before_filter :verify_authenticity_token, :check_if_login_required
 
+  @@log = "logtest"
 
   def index
     if request.post?
